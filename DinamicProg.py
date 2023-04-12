cda925a437f543ec5968ab26aa31e2a7500250e1
leonardo <leonardo.pampaloni1@stud.unifi.it>
2023-04-12 16:59:00 +0200

    lesson fibonacci

--- DinamicProg.py
+++ DinamicProg.py
@@ -1,10 +1,20 @@
-def fib(n):
+def fibb(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fib(n - 1) + fib(n - 2)
+        return fibb(n - 1) + fibb(n - 2)
+
+
+def fibTab(n):
+    table = [0 for i in range(n + 1)]
+    table[0] = 0
+    table[1] = 1
+    table[2] = 1
+    for j in range(3, n + 1):
+        table[j] = table[j - 1] + table[j - 2]
+    return table[n]
 
 
 def calculateFibonacci(n):
@@ -21,5 +31,36 @@ def calculateFibonacciRec(memoize, n):
     return memoize[n]
 
 
-print(fib(7))
+def fib(n, diz):  # prof metodo top down
+    if n == 0 or n == 1:
+        return 1
+    else:
+        if n - 1 in diz:
+            a = diz[n - 1]
+        else:
+            a = fib(n - 1, diz)
+        if n - 2 in diz:
+            b = diz[n - 2]
+        else:
+            b = fib(n - 2, diz)
+            c = a + b
+        diz[n] = c
+        return c
+
+
+def minCoinNum(x): # roba pallosa
+    
+    MN = [0 for i in range(x + 1)]
+    coins = [1, 2, 5, 10, 20, 50, 100, 200, 500]
+    for i in range(x+1):
+        MN[i] = 123456789
+        for j in range(8):  # ?
+            if coins[j] <= i and MN[i] > 1 + MN[i - coins[j]]:
+                MN[i] = 1 + MN[i - coins[j]]
+    return MN[x]
+
+
+print(fibb(7))
 print(calculateFibonacci(8))
+print(fibTab(7))
+print(minCoinNum(7))
